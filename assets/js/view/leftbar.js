a8f404d8f285ed5760f4362e76bf60611790c565
Diksha <dg.dikshag2612@gmail.com>
2020-10-07 22:22:05 +0530

    add event listeners on left bar

--- assets/js/view/leftbar.js
+++ assets/js/view/leftbar.js
@@ -1,35 +1,85 @@
 let left_icons = {
-  "fa-pencil": { tooltip: "Edit", data: "", modal: false },
-  "fa-eraser": { tooltip: "Erase", data: "", modal: false },
-  "fa-refresh": { tooltip: "Clear", data: "", modal: false },
-  "fa-eyedropper": { tooltip: "Paint", data: "", modal: false },
-  "fa-th": { tooltip: "Grid", data: "", modal: false },
-  "fa-undo": { tooltip: "Undo", data: "", modal: false },
-  "fa-repeat": { tooltip: "Redo", data: "", modal: false },
-  "fa-floppy-o": { tooltip: "Save", data: "", modal: false },
-  "fa-download": { tooltip: "Download", data: "", modal: false },
-  "fa-question-circle": { tooltip: "Help", data: "", modal: false },
+  "fa-pencil": { tooltip: "Edit", callback: "edit" },
+  "fa-eraser": { tooltip: "Erase", callback: "erase" },
+  "fa-refresh": { tooltip: "Clear", callback: "clearAll" },
+  "fa-eyedropper": { tooltip: "Paint", callback: "paint" },
+  "fa-th": { tooltip: "Grid", callback: "grid" },
+  "fa-undo": { tooltip: "Undo", callback: "undo" },
+  "fa-repeat": { tooltip: "Redo", callback: "redo" },
+  "fa-floppy-o": { tooltip: "Save", callback: "save" },
+  "fa-download": { tooltip: "Download", callback: "download" },
+  "fa-question-circle": { tooltip: "Help", callback: "help" },
 };
 let leftbar = document.querySelector("#leftbar");
-
-// let frag = document.createDocumentFragment();
 let ul = document.createElement("ul");
 let icon_keys = Object.keys(left_icons);
 icon_keys.forEach((key) => {
   let li = document.createElement("li");
   let itag = document.createElement("i");
   itag.className = `fa ${key}`;
+  if (left_icons[key].callback) {
+    li.setAttribute("data-callback", left_icons[key].callback);
+    itag.setAttribute("data-callback", left_icons[key].callback);
+  }
   li.appendChild(itag);
   let tooltip = document.createElement("span");
   tooltip.className = `tooltiptext`;
   tooltip.innerHTML = left_icons[key].tooltip;
   li.appendChild(tooltip);
   li.className = "leftbar_icon";
-  // li.tooltip = left_icons[key].tooltip;
   ul.appendChild(li);
 });
 leftbar.appendChild(ul);
 leftbar.addEventListener("click", leftbarevents);
-function leftbarevents() {
-  console.log(event);
+function leftbarevents(e) {
+  let activeEl = document.querySelector(".active");
+  if (activeEl) activeEl.classList.remove("active");
+
+  if (e.target.localName == "li") e.target.classList.add("active");
+  else e.target.parentNode.classList.add("active");
+
+  if (e.target.dataset && e.target.dataset.callback) {
+    let callback = e.target.dataset.callback;
+    window[callback]();
+  }
 }
+function edit() {
+  console.log("edit clicked");
+}
+function erase() {
+  console.log("erase clicked");
+}
+function clearAll() {
+  console.log("clearAll clicked");
+}
+function paint() {
+  console.log("paint clicked");
+}
+function grid() {
+  console.log("grid clicked");
+}
+function undo() {
+  console.log("undo clicked");
+}
+function redo() {
+  console.log("redo clicked");
+}
+function save() {
+  console.log("save clicked");
+}
+function download() {
+  console.log("download clicked");
+}
+function help() {
+  console.log("help clicked");
+}
+function openModal() {}
+// function executeFunctionByName(functionName, context /*, args */) {
+//   var args = Array.prototype.slice.call(arguments, 2);
+//   var namespaces = functionName.split(".");
+//   var func = namespaces.pop();
+//   for (var i = 0; i < namespaces.length; i++) {
+//     context = context[namespaces[i]];
+//   }
+//   return context[func].apply(context, args);
+// }
